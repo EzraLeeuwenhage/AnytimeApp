4f0065e593c735949dc0a6cf85376b2121e3b75c
EzraLeeuwenhage <ezra1999@hotmail.nl>
2023-01-03 14:19:51 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore'; 
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBlyizxiyCaf4WMZqgCCUmyx6dPRzGV-CI",
+  authDomain: "anytimeapp-622be.firebaseapp.com",
+  databaseURL: "https://anytimeapp-622be-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "anytimeapp-622be",
+  storageBucket: "anytimeapp-622be.appspot.com",
+  messagingSenderId: "904189972882",
+  appId: "1:904189972882:web:6648486cf585818ef7714b",
+  measurementId: "G-54EDQGX11H"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
